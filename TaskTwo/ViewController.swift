9338db4f245da4a4370390b86155a07b3136feca
Raju Gupta <itsrajura@gmail.com>
2020-10-01 00:14:38 +0530

    Initial Commit

---
+++ TaskTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskTwo
+//
+//  Created by Raju Gupta on 01/10/20.
+//  Copyright © 2020 Raju Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
